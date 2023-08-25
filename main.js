842a3883a82ef2747814a5745008f9c4425dfcab
swapnil <swapnilgawali244@gmail.com>
2023-08-25 18:45:59 +0530

    added changes

--- main.js
+++ main.js
@@ -5,9 +5,35 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 const discussionTitle = github.context.payload.discussion.title;
-const discussionLabels = github.context.payload.discussion.labels ? github.context.payload.discussion.labels.map(label => label.name) : [];
 const discussionBody = github.context.payload.discussion.body;
 
+const query = `
+    query {
+      repository(owner: "${github.context.repo.owner}", name: "${github.context.repo.repo}") {
+        discussion(number: ${discussionNum}) {
+          title
+          body
+          labels(first: 10) {
+            nodes {
+              name
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  // Make a request to the GitHub GraphQL API using octokit
+  const octokit = github.getOctokit(core.getInput("PAT")); // Assuming you pass your repository token as an input
+
+  // Use .graphql instead of .graphql.query for making the request
+  octokit.graphql(query).then((response) => {
+    // Extract labels from the response
+    const discussionLabels = response.repository.discussion.labels.nodes.map(
+      (node) => node.name
+    );
+    console.log("Discussion Labels:", discussionLabels);
+    
 const promptJson = {
   Title: discussionTitle,
   Labels: discussionLabels,
@@ -26,3 +52,4 @@ if (validationResult.valid) {
   console.log('Generated prompt JSON is invalid.');
   console.log('Validation errors:', validationResult.errors);
 }
+});
